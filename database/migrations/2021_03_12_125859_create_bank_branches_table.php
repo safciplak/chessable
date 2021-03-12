3c1c5894e3cec3cc88d0bc546e666cede1261f0d
Safak Ciplak <safakciplak1990@gmail.com>
2021-03-12 16:14:48 +0300

    add migrations

---
+++ database/migrations/2021_03_12_125859_create_bank_branches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_branches', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('bank_id');
+            $table->point('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_branches');
+    }
+}
